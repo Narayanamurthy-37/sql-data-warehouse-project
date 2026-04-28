044e1da5b0b997b55a22c361ff6f06c7005cc350
Narayanamurthy-37 <narayanamurthy375@gmail.com>
2026-04-28 13:25:50 +0530

    Create load_bronze procedure for data loading
    This procedure loads data into various bronze tables from CSV files, handling errors during the process.

--- Script/Bronze/load_bronze.sql
+++ Script/Bronze/load_bronze.sql
@@ -1,1 +1,80 @@
 
+CREATE OR ALTER PROCEDURE bronze.load_bronze
+AS
+BEGIN
+    BEGIN TRY
+        PRINT 'Starting Bronze Load';
+
+        -- CRM CUSTOMER
+        TRUNCATE TABLE bronze.crm_cust_info;
+        BULK INSERT bronze.crm_cust_info
+        FROM 'C:\Users\SJC-Library\Desktop\Datasets\DW\cust_info.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+
+        -- CRM PRODUCT
+        TRUNCATE TABLE bronze.crm_prd_info;
+        BULK INSERT bronze.crm_prd_info
+        FROM 'C:\Users\SJC-Library\Desktop\Datasets\DW\prd_info.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+
+        -- CRM SALES
+        TRUNCATE TABLE bronze.crm_sales_details;
+        BULK INSERT bronze.crm_sales_details
+        FROM 'C:\Users\SJC-Library\Desktop\Datasets\DW\sales_details.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+
+        -- ERP LOCATION
+        TRUNCATE TABLE bronze.erp_loc_a101;
+        BULK INSERT bronze.erp_loc_a101
+        FROM 'C:\Users\SJC-Library\Desktop\Datasets\DW\LOC_A101.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+
+        -- ERP CUSTOMER
+        TRUNCATE TABLE bronze.erp_cust_az12;
+        BULK INSERT bronze.erp_cust_az12
+        FROM 'C:\Users\SJC-Library\Desktop\Datasets\DW\CUST_AZ12.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+
+        -- ERP PRODUCT CATEGORY
+        TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+        BULK INSERT bronze.erp_px_cat_g1v2
+        FROM 'C:\Users\SJC-Library\Desktop\Datasets\DW\PX_CAT_G1V2.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+
+        PRINT 'Bronze Load Completed Successfully';
+
+    END TRY
+    BEGIN CATCH
+        PRINT 'Bronze Load Failed';
+        PRINT ERROR_MESSAGE();
+    END CATCH
+END;
+
+
+EXEC bronze.load_bronze;  -- this should run in separate page—
+
+
